41b9b46650740a05bea660155c98516d35e97af3
Ariel Rodriguez <arielrodriguez@Ariels-Mac-mini.local>
2021-02-25 00:39:07 -0500

    working on nav and landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "tailwindcss/tailwind.css";
+import { ThemeProvider } from "../components/theme/themeContext";
+import Layout from "./layout/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
+import LandingHero from "../components/LandingHero";
+import Toggle from "../components/theme/Toggle";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-red-500 dark:bg-gray-800 duration-500">
       <Head>
-        <title>Create Next App</title>
+        <title>Ryan Zarb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>testging</h1>
+      <LandingHero />
     </div>
   );
 }
