df441ac250d68bb0d2de00ec8c335b35b63797fa
Ariel Rodriguez <arielrodriguez@Ariels-Mac-mini.local>
2021-02-26 17:16:36 -0500

    updating links for cards, they now open on seperate page

--- components/Card.js
+++ components/Card.js
@@ -31,14 +31,14 @@ export default function Card({ recentSales }) {
                 title,
                 url,
               } = item;
-              console.log("url", url);
+
               return (
                 <div
                   key={id}
                   className="flex flex-col rounded-lg shadow-lg overflow-hidden"
                 >
                   <div className="flex-shrink-0">
-                    <a href="www.google.com" target="_blank">
+                    <a href={url} target="_blank">
                       <img
                         className="h-96 w-full object-cover"
                         src={cardImage.url}
@@ -47,42 +47,34 @@ export default function Card({ recentSales }) {
                     </a>
                   </div>
 
-                  <a href="https://www.zillow.com/" target="_blank">
-                    <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                      <div className="flex-1">
-                        <p className="text-sm font-medium text-green-600">
-                          <a href="#" className="hover:underline">
-                            Sold
-                          </a>
+                  <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                    <div className="flex-1">
+                      <p className="text-sm font-medium text-green-600">Sold</p>
+                      <a href={url} target="_blank" className="block mt-2">
+                        <p className="text-xl font-semibold text-gray-900">
+                          {title}
                         </p>
-                        <a href="#" className="block mt-2">
-                          <p className="text-xl font-semibold text-gray-900">
-                            {title}
+                        <div className="text-gray-600 text-xs uppercase font-semibold tracking-wide">
+                          <p>
+                            {beds} beds &bull; {baths} baths
                           </p>
-                          <div className="text-gray-600 text-xs uppercase font-semibold tracking-wide">
-                            <p>
-                              {beds} beds &bull; {baths} baths
-                            </p>
-                          </div>
-                          <p className="mt-3 text-base text-gray-500">
-                            {description}
-                          </p>
-                        </a>
-                      </div>
-                      <div className=" flex items-center">
-                        <div className="mt-4">
-                          <p className="text-sm font-medium text-green-600">
-                            <a href="#" className="hover:underline">
-                              ${salePrice}
-                            </a>
-                          </p>
-                          <div className="flex space-x-1 text-sm text-gray-500">
-                            <time dateTime="2020-03-16">{date}</time>
-                          </div>
+                        </div>
+                        <p className="mt-3 text-base text-gray-500">
+                          {description}
+                        </p>
+                      </a>
+                    </div>
+                    <div className=" flex items-center">
+                      <div className="mt-4">
+                        <p className="text-sm font-medium text-green-600">
+                          ${salePrice}
+                        </p>
+                        <div className="flex space-x-1 text-sm text-gray-500">
+                          <time dateTime="2020-03-16">{date}</time>
                         </div>
                       </div>
                     </div>
-                  </a>
+                  </div>
                 </div>
               );
             })}
